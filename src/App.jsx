ae44ca8208cff52ee6a6b1fd7433f4bff6c6d510
goro-ne <hayao56@gmail.com>
2024-07-30 11:05:06 +0900

    ReactRouterの画面遷移テスト

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+
 import './App.css';
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <BrowserRouter>
+      <div>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={
+          <Home />
+        }>
+        </Route>
+        <Route path="/page1" element={
+          <Page1 />
+        }>
+        </Route>
+        <Route path="/page2" element={
+          <Page2 />
+        }>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
